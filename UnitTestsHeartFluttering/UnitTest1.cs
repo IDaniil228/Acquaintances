de99eb17ea722137bbc8f626b23017f60f309516
IDaniil228 <99666220+IDaniil228@users.noreply.github.com>
2024-04-27 18:35:36 +0300

    add tests

---
+++ UnitTestsHeartFluttering/UnitTest1.cs
@@ -0,0 +1,14 @@
+using HeartFluttering;
+
+
+namespace UnitTestsHeartFluttering
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
